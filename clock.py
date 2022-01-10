285da657255ef353c626548fb3918394bd2b1d86
qwertyytrewqaka <82452752+qwertyytrewqaka@users.noreply.github.com>
2022-01-11 01:09:46 +0900

    Update clock.py

--- clock.py
+++ clock.py
@@ -7,7 +7,7 @@ sched = BlockingScheduler()
 def timed_job():
     main.main()
 
-sched.add_job(timed_job, 'interval', seconds=10, jitter=1, max_instances=2)
+sched.add_job(timed_job, 'interval', seconds=20, jitter=1, max_instances=2)
 
 
 sched.start()
