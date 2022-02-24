b9adbfdf23c0d389cc1f458723c0a440a0710dec
qwertyytrewqaka <82452752+qwertyytrewqaka@users.noreply.github.com>
2022-02-25 01:41:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import constants
 
 def nt_notify():
-    with requests.get("https://newtoki122.com/toki_free", headers=constants.headers) as req:
+    with requests.get("https://newtoki123.com/toki_free", headers=constants.headers) as req:
         called = False
         constants.k_number = 0
         k_list = []
