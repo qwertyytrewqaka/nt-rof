08f6f20f042796ff1b31de0631656d4b7d0df1bc
qwertyytrewqaka <82452752+qwertyytrewqaka@users.noreply.github.com>
2022-03-30 22:29:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def nt_notify():
 
         number = posts[constants.latest_number].select("div.wr-num.hidden-xs")[0].text
         time = posts[constants.latest_number].select("div.wr-date.hidden-xs")[0].text[1:].rstrip()
-        downloads = posts[constants.latest_number].select("div.wr-down.hidden-xs")[0].text[1:].rstrip()
+        downloads = posts[constants.latest_number].select("div.wr-down.hidden-xs")[0].text[1:].rstrip().lstrip()
         category = posts[constants.latest_number].select("div.wr-subject > a > span.tack-icon")[0].text
         talk_temp = posts[constants.latest_number].select("div.wr-subject > a > span.count.orangered.hidden-xs")
         talk = talk_temp[0].text if len(talk_temp) != 0 else ""
@@ -64,7 +64,7 @@ def nt_notify():
 
             number = posts[constants.latest_number+1].select("div.wr-num.hidden-xs")[0].text
             time = posts[constants.latest_number+1].select("div.wr-date.hidden-xs")[0].text[1:].rstrip()
-            downloads = posts[constants.latest_number+1].select("div.wr-down.hidden-xs")[0].text[1:].rstrip()
+            downloads = posts[constants.latest_number+1].select("div.wr-down.hidden-xs")[0].text[1:].rstrip().lstrip()
             category = posts[constants.latest_number+1].select("div.wr-subject > a > span.tack-icon")[0].text
             talk_temp = posts[constants.latest_number+1].select("div.wr-subject > a > span.count.orangered.hidden-xs")
             talk = talk_temp[0].text if len(talk_temp) != 0 else ""
